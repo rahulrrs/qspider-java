5068ee3b7578612bc19129fad8187d88ecc2ab6c
Rahul Reji R <rahulrrreji@gmail.com>
2026-03-17 10:52:52 +0530

    17-03-26

--- Primealt.java
+++ Primealt.java
@@ -8,6 +8,14 @@ public class Primealt {
         System.out.println(prime(number));
     }
     public static int prime(int number) {
-        
+        if (number <= 1) {
+            return 0;
+        }
+        for (int i = 2; i * i <= number; i++) {
+            if (number % i == 0) {
+                return 0;
+            }
+        }
+        return 1;
     }
 }
